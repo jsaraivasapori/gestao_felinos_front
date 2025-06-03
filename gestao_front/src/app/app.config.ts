d33b7833a9d29c03d3b89ebd69dc6ef7358e631b
joaosaraivasapori@gmail.com <joaosaraivasapori@gmail.com>
2025-06-02 22:18:32 -0300

    configurado datePicker bem como setado a regiao

--- gestao_front/src/app/app.config.ts
+++ gestao_front/src/app/app.config.ts
@@ -1,17 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideNgxMask } from 'ngx-mask';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideAnimationsAsync(),
     provideNgxMask(),
     provideHttpClient(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    importProvidersFrom(MatNativeDateModule),
   ],
 };

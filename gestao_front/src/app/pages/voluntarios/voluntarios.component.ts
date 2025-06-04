113d0b5a3a62c6a651d6d2c284fac1835eaf0b3f
joaosaraivasapori@gmail.com <joaosaraivasapori@gmail.com>
2025-06-04 00:11:38 -0300

    continuando

--- gestao_front/src/app/pages/voluntarios/voluntarios.component.ts
+++ gestao_front/src/app/pages/voluntarios/voluntarios.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { TableReOrderableColumnsComponent } from '../../components/table-re-orderable-columns/table-re-orderable-columns.component';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FelinoService } from '../../services/felinoService/felino.service';
+import { Router, ActivatedRoute, RouterOutlet } from '@angular/router';
 import { SharedService } from '../../services/shared.service';
 import { Voluntario } from '../../models/voluntarioModel/voluntario-model';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,12 +8,11 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-voluntarios',
   standalone: true,
-  imports: [TableReOrderableColumnsComponent, MatButtonModule],
+  imports: [TableReOrderableColumnsComponent, MatButtonModule, RouterOutlet],
   templateUrl: './voluntarios.component.html',
   styleUrl: './voluntarios.component.scss',
 })
 export class VoluntariosComponent {
-  showTable: boolean = true;
   data: any = [
     { nome: 'João Vitor', idade: 25, bairro: 'Todos os Santos Prolongamento' },
     { nome: 'Rosangela', idade: 51, bairro: 'Todos os Santos Prolongamento' },
@@ -22,22 +20,25 @@ export class VoluntariosComponent {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private sharedService: SharedService,
-    private felinoService: FelinoService
+    private sharedService: SharedService
   ) {}
+  /**
+   * Metodo getter que retorna true se estiver na rota de formulario
+   */
   get isFormRoute(): boolean {
     return this.router.url === '/home/voluntarios/form';
   }
 
-  toEditvolunteero(volunteerToEdit: Voluntario) {
-    this.showTable = false;
+  toEditVolunteero(volunteerToEdit: Voluntario) {
     this.sharedService.setData('currentVolunteer', volunteerToEdit);
+    this.router.navigate(['form'], { relativeTo: this.route });
   }
-  toDeletevolunteer(data: Voluntario) {
+  toDeleteVolunteer(data: Voluntario) {
     //chamar modal de confirmação para apagar
+    // alem de passar o id passar o nome do voluntario
     console.log('Estou deletando:', data);
   }
-  addNewvolunteer(): void {
+  addNewVolunteer(): void {
     this.sharedService.clearData('currentVolunteer');
   }
 }

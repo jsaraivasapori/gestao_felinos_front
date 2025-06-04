f493156f4bc1b85c40234076d48f0d04a1b4d9db
joaosaraivasapori@gmail.com <joaosaraivasapori@gmail.com>
2025-06-03 23:58:46 -0300

    voluntarios form inicio

---
+++ gestao_front/src/app/pages/voluntarios/edit-voluntarios/voluntarios-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voluntarios-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './voluntarios-form.component.html',
+  styleUrl: './voluntarios-form.component.scss'
+})
+export class VoluntariosFormComponent {
+
+}

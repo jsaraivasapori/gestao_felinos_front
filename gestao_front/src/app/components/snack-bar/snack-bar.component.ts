a5ef309442a3cedc3029b249d5ab95055d54f6b8
joaosaraivasapori@gmail.com <joaosaraivasapori@gmail.com>
2025-06-09 23:32:57 -0300

    snackBar criado

---
+++ gestao_front/src/app/components/snack-bar/snack-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-snack-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './snack-bar.component.html',
+  styleUrl: './snack-bar.component.scss',
+})
+export class SnackBarComponent {
+  data = inject(MAT_SNACK_BAR_DATA);
+}

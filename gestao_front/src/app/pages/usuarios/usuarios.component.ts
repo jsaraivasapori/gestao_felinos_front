79a441431225acfe0e4c58decd5a0a2274bdf174
joaosaraivasapori@gmail.com <joaosaraivasapori@gmail.com>
2025-06-12 20:03:40 -0300

    usuarios

--- gestao_front/src/app/pages/usuarios/usuarios.component.ts
+++ gestao_front/src/app/pages/usuarios/usuarios.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { UsuarioCreate } from '../../models/usuarioModel/usuarios-model';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent,
+    MatDividerModule,
+    MatButtonModule,
+    MatPaginatorModule,
+  ],
   templateUrl: './usuarios.component.html',
-  styleUrl: './usuarios.component.scss'
+  styleUrl: './usuarios.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsuariosComponent {
+  usuarios: UsuarioCreate[] = [
+    {
+      nome: 'João Vitor',
+      login: 'jvss',
+      senha: '****',
+      perfil: 'Administrador',
+    },
+    {
+      nome: 'Rosangela',
+      login: 'rosademinas',
+      senha: '*****',
+      perfil: 'Gerencial',
+    },
+    {
+      nome: 'Pablo',
+      login: 'pablodeminas',
+      senha: '*****',
+      perfil: 'Gerencial',
+    },
+    {
+      nome: 'Pablo',
+      login: 'pablodeminas',
+      senha: '*****',
+      perfil: 'Gerencial',
+    },
+    {
+      nome: 'Pablo',
+      login: 'pablodeminas',
+      senha: '*****',
+      perfil: 'Gerencial',
+    },
+    {
+      nome: 'Pablo',
+      login: 'pablodeminas',
+      senha: '*****',
+      perfil: 'Gerencial',
+    },
+    {
+      nome: 'Pablo',
+      login: 'pablodeminas',
+      senha: '*****',
+      perfil: 'Gerencial',
+    },
+  ];
 
+  // Sinais para controle de paginação
+  pageSize = 6;
+  currentPage = signal(0);
+  // Usuários paginados (computado reativo)
+  pagedUsuarios = computed(() => {
+    const startIndex = this.currentPage() * this.pageSize;
+    return this.usuarios.slice(startIndex, startIndex + this.pageSize);
+  });
+
+  // Manipulador de mudança de página
+  onPageChange(event: PageEvent) {
+    this.currentPage.set(event.pageIndex);
+  }
 }

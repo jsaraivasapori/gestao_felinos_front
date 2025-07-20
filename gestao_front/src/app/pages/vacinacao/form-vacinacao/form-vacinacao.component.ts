a75ff1b104273701bdeccd1b6ce86ea3047b4beb
João Vitor <joaosaraivasapori@gmail.com>
2025-07-19 22:57:25 -0300

    form-vacinacao concluido

--- gestao_front/src/app/pages/vacinacao/form-vacinacao/form-vacinacao.component.ts
+++ gestao_front/src/app/pages/vacinacao/form-vacinacao/form-vacinacao.component.ts
@@ -74,7 +74,7 @@ export class FormVacinacaoComponent implements OnInit {
         '',
         [Validators.required, Validators.pattern('^[1-9][0-9]*$')],
       ],
-      DataProximaVacina: [''],
+      dataProximaVacina: [new Date()],
       intervaloEntreDosesEmDias: ['', Validators.pattern('^[0-9][0-9]*$')],
       requerReforcoAnual: [false, Validators.required],
     });
@@ -99,8 +99,8 @@ export class FormVacinacaoComponent implements OnInit {
   get dosesNecessarias() {
     return this.vacinacaoForm.get('dosesNecessarias') as FormControl;
   }
-  get DataProximaVacina() {
-    return this.vacinacaoForm.get('DataProximaVacina') as FormControl;
+  get dataProximaVacina() {
+    return this.vacinacaoForm.get('dataProximaVacina') as FormControl;
   }
   get intervaloEntreDosesEmDias() {
     return this.vacinacaoForm.get('intervaloEntreDosesEmDias') as FormControl;
@@ -108,86 +108,12 @@ export class FormVacinacaoComponent implements OnInit {
   get requerReforcoAnual() {
     return this.vacinacaoForm.get('requerReforcoAnual') as FormControl;
   }
-  onFormSubmitted(formValue: any) {
+  onFormSubmitted() {
+    const formValue = this.vacinacaoForm.value;
     console.log(formValue);
     this.router.navigate(['home', 'vacinas']);
   }
   onCancel() {
     this.router.navigate(['home', 'vacinas']);
   }
-
-  // private buildForm(
-  //   felinos: FelinoInfoBasic[],
-  //   vaccines: Vaccine[]
-  // ): FormField[] {
-  //   return [
-  //     {
-  //       name: 'felino',
-  //       label: 'Felino',
-  //       type: 'select',
-  //       options: felinos.map((f) => ({ value: f.id, label: f.nome })),
-  //       validators: [Validators.required],
-  //       errorMessages: { required: 'Campo obrigatório' },
-  //     },
-  //     {
-  //       name: 'vacina',
-  //       label: 'Vacina',
-  //       type: 'select',
-  //       options: vaccines.map((v) => ({ value: v.id, label: v.nome })),
-  //       validators: [Validators.required],
-  //       errorMessages: { required: 'Campo obrigatório' },
-  //     },
-
-  //     {
-  //       name: 'lote',
-  //       label: 'Lote',
-  //       type: 'text',
-  //       validators: [Validators.required],
-  //       errorMessages: { required: 'Campo obrigatório' },
-  //     },
-  //     {
-  //       name: 'medVet',
-  //       label: 'Médico (a) veterinário (a)',
-  //       type: 'text',
-  //       validators: [Validators.required],
-  //       errorMessages: { required: 'Campo obrigatório' },
-  //     },
-  //     {
-  //       name: 'valorPago',
-  //       label: 'Valor da vacina',
-  //       type: 'text',
-  //       validators: [Validators.required, Validators.min(0.1)],
-  //       errorMessages: { required: 'Campo obrigatório', min: 'Valor inválido' },
-  //     },
-  //     {
-  //       name: 'dosesNecessarias',
-  //       label: 'Doses necessárias',
-  //       type: 'text',
-  //       validators: [Validators.required, Validators.pattern('^[1-9][0-9]*$')],
-  //       errorMessages: {
-  //         required: 'Campo obrigatório',
-  //         pattern: 'Campo inválido',
-  //       },
-  //     },
-  //     {
-  //       name: 'DataProximaVacina',
-  //       label: 'Próxima dose',
-  //       type: 'datePicker',
-  //     },
-  //     {
-  //       name: 'intervaloEntreDosesEmDias',
-  //       label: 'Intervalo entre as doses em dias',
-  //       type: 'text',
-  //       validators: [Validators.pattern('^[0-9][0-9]*$')],
-  //       errorMessages: { pattern: 'Campo inválido' },
-  //     },
-  //     {
-  //       name: 'requerReforcoAnual',
-  //       label: 'Reforço anaul',
-  //       type: 'slide-toggle',
-  //       validators: [Validators.required],
-  //       errorMessages: { required: 'Campo obrigatório' },
-  //     },
-  //   ];
-  // }
 }
